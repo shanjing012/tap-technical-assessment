b3d32f2d072359db0610c745572b098654cdfe3e
shanjing012 <33203151+shanjing012@users.noreply.github.com>
2020-03-25 17:19:33 +0800

    Add student encouragement bonus grant household search

--- src/main/java/com/assessment/repository/HouseholdRepository.java
+++ src/main/java/com/assessment/repository/HouseholdRepository.java
@@ -2,14 +2,39 @@ package com.assessment.repository;
 
 import com.assessment.entity.HouseholdEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Repository
 @Transactional
 @Component
 public interface HouseholdRepository extends JpaRepository<HouseholdEntity, Long> {
+
+    @Query(
+            "SELECT h FROM HouseholdEntity h INNER JOIN h.memberEntityList m " +
+            "GROUP BY h.id " +
+            "HAVING CAST(SUM(m.annualIncome) as long) < :annualIncome " +
+            "AND CAST(MIN(DATE_FORMAT(FROM_DAYS(DATEDIFF(now(), m.dateOfBirth)), '%Y')) as long) < :age"
+    )
+    List<HouseholdEntity> selectStudentEncouragementBonusHousehold(Long age, Long annualIncome);
+
+    @Query(
+            "SELECT h FROM HouseholdEntity h INNER JOIN h.memberEntityList m " +
+                    "GROUP BY h.id " +
+                    "HAVING CAST(SUM(m.annualIncome) as long) < :annualIncome " +
+                    "AND MIN(DATE_FORMAT(FROM_DAYS(DATEDIFF(now(), m.dateOfBirth)), '%Y')) < :age"
+    )
+    List<HouseholdEntity> selectFamilyTogethernessSchemeHousehold(Integer annualIncome, Long age);
+
+    List<HouseholdEntity> selectElderBonusHousehold(Integer annualIncome, Long age);
+
+    List<HouseholdEntity> selectBabySunshineGrantHousehold(Integer annualIncome, Long age);
+
+    List<HouseholdEntity> selectYoloGSTGrantHousehold(Integer annualIncome, Long age);
 
 }
--- src/main/java/com/assessment/service/HouseholdService.java
+++ src/main/java/com/assessment/service/HouseholdService.java
@@ -11,12 +11,20 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.persistence.EntityManager;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Component
 public class HouseholdService {
+
+    @Autowired
+    EntityManager entityManager;
 
     @Autowired
     private HouseholdRepository householdRepository;
@@ -72,7 +80,28 @@ public class HouseholdService {
         return optionalHouseholdEntity.orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid household ID"));
     }
 
-//    public List<Household> findHouseholdBasedOn(Integer maximumAge, Integer minimumAge, Boolean hasMarriedCouple, Integer householdSize, Integer totalIncome) {
-//
-//    }
+    public List<Household> showStudentEncouragementBonusRecipients(Long ageLessThan, Long annualIncomeLessThan) {
+
+        List<HouseholdEntity> householdEntityList = householdRepository.selectStudentEncouragementBonusHousehold(ageLessThan, annualIncomeLessThan);
+        List<Household> householdList = new ArrayList<>();
+        for(HouseholdEntity householdEntity : householdEntityList) {
+            //Convert householdentity to household and populate members who are less than 16 years old
+            Household household = new Household(householdEntity.getId(), householdEntity.getHouseholdType());
+            //Filter members who are less than 16 years old
+            household.setMemberList(householdEntity.getMemberEntityList()
+                    .stream()
+                    .filter(memberEntity -> memberEntity.getDateOfBirth().until(LocalDate.now()).get(ChronoUnit.YEARS) < ageLessThan)
+                    .map(memberEntity -> new Member(
+                        memberEntity.getId(),
+                        memberEntity.getName(),
+                        memberEntity.getGender(),
+                        memberEntity.getMaritalStatus(),
+                        memberEntity.getSpouse() != null ? memberEntity.getSpouse().getId() : null,
+                        memberEntity.getAnnualIncome(),
+                        memberEntity.getDateOfBirth()
+                    )).collect(Collectors.toList()));
+            householdList.add(household);
+        }
+        return householdList;
+    }
 }
